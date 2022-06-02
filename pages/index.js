b78f1ca3d70b0adb9344efb554533b264bb25f9e
Popo047 <sanchay2234@gmail.com>
2022-06-02 14:37:27 +0530

    getStaticProps Implementation

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,30 @@
 import Link from "next/link";
 
-export default function HomePage() {
+export default function HomePage(props) {
+	const { products } = props;
+
+	console.log(products);
+
 	return (
 		<div>
 			<h1>HomePage</h1>
 			<ul>
-				<li>
-					<Link href="/portfolio">Portfolio Page</Link>
-				</li>
-				<li>
-					<Link href="/clients">Clients Page</Link>
-				</li>
+				<li>p1</li>
+				<li>p2</li>
+				<li>p3</li>
 			</ul>
 		</div>
 	);
+}
+
+export async function getStaticProps() {
+	//code that is not visible on client side, fetches data through props to this component
+	//getStaticProps is called before executing the component
+	//props must be an object (props is the key)
+
+	return {
+		props: {
+			products: [{ id: "p1", title: "Product1" }],
+		},
+	};
 }
